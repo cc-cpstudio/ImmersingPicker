3a17c9f06e393c9e5942ab3facae037221a99434
programmerCC <18770248413@163.com>
2026-01-31 19:11:53 +0800

    初始化仓库

---
+++ src/main/kotlin/module-info.java
@@ -0,0 +1,14 @@
+module com.github.immersingeducation.immersingpicker {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires kotlin.stdlib;
+
+    requires org.controlsfx.controls;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+
+    requires tornadofx;
+
+    opens com.github.immersingeducation.immersingpicker to javafx.fxml;
+    exports com.github.immersingeducation.immersingpicker;
+}
