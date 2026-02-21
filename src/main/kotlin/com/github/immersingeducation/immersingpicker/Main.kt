9889ce0cc72599bdf2c88a505427425dc86ddafb
programmerCC <18770248413@163.com>
2026-02-21 17:01:14 +0800

    refractor 修改主函数，自动启动周期性保存功能

--- src/main/kotlin/com/github/immersingeducation/immersingpicker/Main.kt
+++ src/main/kotlin/com/github/immersingeducation/immersingpicker/Main.kt
@@ -1,8 +1,11 @@
 package com.github.immersingeducation.immersingpicker
 
+import com.github.immersingeducation.immersingpicker.data.clazz.PeriodicalClazzStorageUtils
 import com.github.immersingeducation.immersingpicker.launch.ImmersingPicker
+import kotlinx.coroutines.runBlocking
 import tornadofx.*
 
-fun main(args: Array<String>) {
+fun main(args: Array<String>) = runBlocking {
+    PeriodicalClazzStorageUtils.start()
     launch<ImmersingPicker>()
 }
