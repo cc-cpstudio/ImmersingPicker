052d6cf67ec9d0c747eba87228cca4f7c3d4c8c3
programmerCC <18770248413@163.com>
2026-02-21 18:46:37 +0800

    refractor 修改主函数，实现读取班级数据、检查并进入恢复模式等功能

--- src/main/kotlin/com/github/immersingeducation/immersingpicker/Main.kt
+++ src/main/kotlin/com/github/immersingeducation/immersingpicker/Main.kt
@@ -1,11 +1,21 @@
 package com.github.immersingeducation.immersingpicker
 
+import com.github.immersingeducation.immersingpicker.core.Clazz
+import com.github.immersingeducation.immersingpicker.data.clazz.ClazzStorageUtils
 import com.github.immersingeducation.immersingpicker.data.clazz.PeriodicalClazzStorageUtils
 import com.github.immersingeducation.immersingpicker.launch.ImmersingPicker
+import com.github.immersingeducation.immersingpicker.launch.RecoverMode
+import com.github.immersingeducation.immersingpicker.launch.recoverReason
 import kotlinx.coroutines.runBlocking
 import tornadofx.*
 
 fun main(args: Array<String>) = runBlocking {
-    PeriodicalClazzStorageUtils.start()
-    launch<ImmersingPicker>()
+    ClazzStorageUtils.loadClasses()
+    if (Clazz.getCurrentClass() == null) {
+        recoverReason = "当前没有可用班级"
+        launch<RecoverMode>()
+    } else {
+        PeriodicalClazzStorageUtils.start()
+        launch<ImmersingPicker>()
+    }
 }
--- src/main/kotlin/com/github/immersingeducation/immersingpicker/launch/ImmersingPicker.kt
+++ src/main/kotlin/com/github/immersingeducation/immersingpicker/launch/ImmersingPicker.kt
@@ -1,11 +1,12 @@
 package com.github.immersingeducation.immersingpicker.launch
 
-import com.github.immersingeducation.immersingpicker.data.clazz.ClazzStorageUtils
-import com.github.immersingeducation.immersingpicker.exception.NoAvailableClass
 import com.github.immersingeducation.immersingpicker.view.main.MainView
 import com.github.immersingeducation.immersingpicker.view.recover.RecoverModeView
+import javafx.scene.control.Alert
 import javafx.stage.Stage
 import tornadofx.*
+
+var recoverReason = "null"
 
 class RecoverMode: App(RecoverModeView::class) {
     override fun start(stage: Stage) {
@@ -13,20 +14,21 @@ class RecoverMode: App(RecoverModeView::class) {
         stage.apply {
             minWidth = 300.0
         }
+
+        Alert(Alert.AlertType.INFORMATION).apply {
+            title = "ImmersingPicker 恢复模式"
+            headerText = "您现在处于恢复模式"
+            contentText = "进入原因：$recoverReason"
+        } .showAndWait()
     }
 }
 
 class ImmersingPicker: App(MainView::class) {
     override fun start(stage: Stage) {
-        ClazzStorageUtils.loadClasses()
-        try {
-            super.start(stage)
-            stage.apply {
-                minWidth = 800.0
-                minHeight = 600.0
-            }
-        } catch(e: NoAvailableClass) {
-            launch<RecoverMode>()
+        super.start(stage)
+        stage.apply {
+            minWidth = 800.0
+            minHeight = 600.0
         }
     }
 }
